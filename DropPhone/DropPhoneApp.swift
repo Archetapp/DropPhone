7c04e9754f8de81c3597fc90be792c8cd58e6a8e
Jared Davidson (Archetapp) <archetapp@gmail.com>
2023-05-15 08:50:02 +0530

    Initial Commit

---
+++ DropPhone/DropPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropPhoneApp.swift
+//  DropPhone
+//
+//  Created by Jared Davidson on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DropPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
