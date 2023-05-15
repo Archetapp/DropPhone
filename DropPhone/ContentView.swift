6b91b09f79ffbbe3e8372f5040bf16fb49889cc8
Jared Davidson (Archetapp) <archetapp@gmail.com>
2023-05-15 09:05:33 +0530

    V1.0

--- DropPhone/ContentView.swift
+++ DropPhone/ContentView.swift
@@ -6,16 +6,84 @@
 //
 
 import SwiftUI
+import CoreMotion
+
+struct AccelerationView: View {
+    @State private var acceleration: Double = 0.0
+    @State private var isInFreefall = false
+    @State private var unlocked: Bool = false
+    private let motionManager = CMMotionManager()
+    
+    var body: some View {
+        Group {
+            if unlocked == true {
+                ZStack {
+                    Color.green.edgesIgnoringSafeArea(.all)
+                    Text("Unlocked!")
+                        .font(.title)
+                }
+            } else {
+                VStack {
+                    Text("Current Acceleration")
+                        .font(.title)
+                    
+                    Text("\(acceleration)")
+                        .font(.largeTitle)
+                        .padding()
+                    
+                    Text(isInFreefall ? "Freefall Detected" : "")
+                        .font(.headline)
+                        .foregroundColor(.red)
+                }
+            }
+        }
+        .onAppear {
+            startAccelerometerUpdates()
+        }
+        .onDisappear {
+            stopAccelerometerUpdates()
+        }
+    }
+    
+    func startAccelerometerUpdates() {
+        guard motionManager.isAccelerometerAvailable else {
+            print("Accelerometer is not available.")
+            return
+        }
+        
+        motionManager.accelerometerUpdateInterval = 0.1
+        
+        motionManager.startAccelerometerUpdates(to: .main) { (data, error) in
+            if let error = error {
+                print("Error receiving accelerometer data: \(error.localizedDescription)")
+                return
+            }
+            
+            if let acceleration = data?.acceleration {
+                let speed = sqrt(pow(acceleration.x, 2) + pow(acceleration.y, 2) + pow(acceleration.z, 2))
+                
+                self.acceleration = speed
+                
+                if speed < 0.1 { // Adjust the threshold as needed
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                        self.isInFreefall = true
+                        self.unlocked = true
+                    }
+                } else {
+                    self.isInFreefall = false
+                }
+            }
+        }
+    }
+    
+    func stopAccelerometerUpdates() {
+        motionManager.stopAccelerometerUpdates()
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        AccelerationView()
     }
 }
 
